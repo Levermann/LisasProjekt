7d4a3c746cfacb5cfa1b99bde7447fcf017564cb
Lisa <lisa.stolz@live.de>
2020-07-08 16:39:42 +0200

    added XmlSchemaValidator

--- Projekt/Semester6/src/XmlValidation/XmlSchemaValidator.java
+++ Projekt/Semester6/src/XmlValidation/XmlSchemaValidator.java
@@ -1,25 +1,61 @@
 package XmlValidation;
 
-import jdk.internal.org.xml.sax.SAXException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class XmlSchemaValidator {
-    public static void main(String[] args) {
 
-        System.out.println("EmployeeRequest.xml validates against Employee.xsd? "+validateXMLSchema("Employee.xsd", "EmployeeRequest.xml"));
-        System.out.println("EmployeeResponse.xml validates against Employee.xsd? "+validateXMLSchema("Employee.xsd", "EmployeeResponse.xml"));
-        System.out.println("employee.xml validates against Employee.xsd? "+validateXMLSchema("Employee.xsd", "employee.xml"));
+    private static final List<String> xmlPaths = Arrays.asList("ITT/MarkUpLanguages/lectureDescriptionSpeech.xml", "ITT/MarkUpLanguages/strongLectureDescriptionSpeech.xml");
 
+        public static void main(String[] args) {
+
+            for(String string : xmlPaths){
+                if(isValidXML(string)){
+                    final String fileName = removeSubstring(string);
+                    System.out.println("the XML File " + fileName + " has passed validation successfully ");
+                }
+            }
+
+        }
+
+
+    private static String removeSubstring(String str) {
+        int startIndex = str.indexOf("ITT/MarkUpLanguages/");
+        int stopIndex = startIndex + "ITT/MarkUpLanguages/".length();
+        StringBuilder builder = new StringBuilder(str);
+        builder.delete(startIndex, stopIndex);
+        return builder.toString();
     }
 
-    public static boolean validateXMLSchema(String xsdPath, String xmlPath){
+    public static boolean isValidXML(String xmlFilePath) {
+        try {
+            DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance();
+            domFactory.setValidating(true);
+            DocumentBuilder builder = domFactory.newDocumentBuilder();
+            Document doc = builder.parse(xmlFilePath);
+            doc.getDocumentElement().normalize();
+
+        } catch (IOException | SAXException | ParserConfigurationException e) {
+            System.out.println("Exception: "+e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+
+    public static boolean isValidXMLAgainstXsdSchema(String xsdPath, String xmlPath){
         try {
             SchemaFactory factory =
                     SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
