3a406cca5f9e8d544a49e1208d4011461ba2ab19
Lisa <lisa.stolz@live.de>
2020-07-08 09:48:01 +0200

    some tests for ITT

---
+++ Projekt/Semester6/src/HttpMethodCaller.java
@@ -0,0 +1,96 @@
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+public class HttpMethodCaller {
+
+    public static void executePost(String targetUrl, String urlParameterss) throws Exception {
+
+        String url = "https://selfsolve.apple.com/wcResults.do";
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        con.setRequestProperty("Accept-Language", "de,en;q=0.5");
+
+        String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+        //Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+    public static byte[] setUrlParameters(String reqOrganization, String paramOrganization, String reqName, String paramName) throws UnsupportedEncodingException {
+        Map<String,String> arguments = new HashMap<>();
+        arguments.put(reqOrganization, paramOrganization);
+        arguments.put(reqName, paramName);
+
+        StringJoiner sj = new StringJoiner("&");
+        for(Map.Entry<String,String> entry : arguments.entrySet())
+            sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "="
+                    + URLEncoder.encode(entry.getValue(), "UTF-8"));
+        return sj.toString().getBytes(StandardCharsets.UTF_8);
+    }
+    public static void getServerResponsePost(String urlClient) throws IOException {
+        URL url = new URL(urlClient);
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection)con;
+        http.setRequestMethod("POST"); // PUT is another valid option
+        http.setDoOutput(true);
+        Map<String,String> arguments = new HashMap<>();
+        arguments.put("username", "root");
+        arguments.put("password", "sjh76HSn!"); // This is a fake password obviously
+        StringJoiner sj = new StringJoiner("&");
+        for(Map.Entry<String,String> entry : arguments.entrySet())
+            sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "="
+                    + URLEncoder.encode(entry.getValue(), "UTF-8"));
+        byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+        http.setFixedLengthStreamingMode(length);
+        http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        http.connect();
+        try(OutputStream os = http.getOutputStream()) {
+            os.write(out);
+        }
+        InputStream in = con.getInputStream();
+        Scanner s = new Scanner(in).useDelimiter("\\A");
+        String result = s.hasNext() ? s.next() : "";
+        String encoding = con.getContentEncoding();
+        encoding = encoding == null ? "UTF-8" : encoding;
+        String body = result;
+        System.out.println("------------------------------------------------------------------------");
+        System.out.println("This is the Server Response " +body + encoding);
+    }
+}
---
+++ Projekt/Semester6/src/MainMethod.java
@@ -0,0 +1,42 @@
+import com.sun.deploy.net.HttpResponse;
+import sun.misc.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+public class MainMethod {
+
+    final static String PATH_GET_METHOD = "http://httpbin.org/";
+    final static String PATH_POST_METHOD = "http://httpbin.org/post/";
+    final static String GET = "GET";
+    final static String POST = "POST";
+
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Ausgabe aus der main()-Methode");
+
+        //SendHttpRequest.executePost(PATH_POST_METHOD,"organisation=HawLa name=li st");
+
+        final byte[] urlParams = HttpMethodCaller.setUrlParameters("organisation", "HawLa", "name", "LS");
+
+        SendHttpRequest.sendPost();
+        HttpMethodCaller.executePost("","");
+
+        // SendHttpRequest.getServerResponses(PATH_POST_METHOD);
+
+
+     //   final SendHttpRequest sendHttpRequest = SendHttpRequest.sendHttpRequest(PATH_POST_METHOD, POST, "organisation=HawLa name=li st");
+        final SendHttpRequest sendHttpRequest = SendHttpRequest.sendHttpRequest(PATH_GET_METHOD, GET,  "");
+    //    System.out.println("This is the response Code from SERVER POST" + sendHttpRequest.getServerResponseStatus());
+      //  System.out.println("This is the response from SERVER POST" + sendHttpRequest.getResponseBody());
+
+
+    }
+
+
+}
